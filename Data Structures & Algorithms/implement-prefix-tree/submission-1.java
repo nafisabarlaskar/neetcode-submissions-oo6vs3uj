4326a380a9ecf35190815a4a340039f802152d4e
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-12 11:19:12 +0530

    Add: implement-prefix-tree - submission-1

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-1.java
@@ -0,0 +1,52 @@
+class PrefixTree {
+    TrieNode root;
+    public PrefixTree() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()){
+            int index = c - 'a';
+            if (node.children[index] == null){
+                node.children[index] = new TrieNode();
+            }
+
+            node = node.children[index];
+        }
+
+        node.word = word;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = prefixSearch(word);
+        return node != null && word.equals(node.word);
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = prefixSearch(prefix);
+        return node != null;
+    }
+
+    private TrieNode prefixSearch(String word){
+        TrieNode node = root;
+        for (char c : word.toCharArray()){
+            int index = c - 'a';
+            if (node.children[index] == null){
+                return null;
+            }
+
+            node = node.children[index];
+        }
+
+        return node;
+    }
+}
+
+class TrieNode {
+    TrieNode[] children;
+    String word;
+    public TrieNode(){
+        children = new TrieNode[26];
+    }
+}
