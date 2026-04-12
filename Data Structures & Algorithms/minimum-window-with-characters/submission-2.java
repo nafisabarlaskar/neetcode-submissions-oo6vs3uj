ef87e0da2788e9e65f6fb88a11ef3f665ff2e09d
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-12 12:18:44 +0530

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.java
@@ -0,0 +1,40 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.length() == 0 || t.length() == 0){
+            return "";
+        }
+
+        Map<Character, Integer> count = new HashMap<>(), tMap = new HashMap<>();
+        for (char c : t.toCharArray()){
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+        int required = tMap.size(), right = 0, left = 0, formed = 0, result[] = {-1, 0, 0};
+        while (right < s.length()){
+            char c = s.charAt(right);
+            count.put(c, count.getOrDefault(c, 0) + 1);
+            if (tMap.containsKey(c) && tMap.get(c) == count.get(c)){
+                formed++;
+            }
+
+            while (left <= right && formed == required){
+                c = s.charAt(left);
+                if (result[0] == -1 || right - left + 1 < result[0]) {
+                    result[0] = right - left + 1;
+                    result[1] = left;
+                    result[2] = right;
+                }
+
+                count.put(c, count.get(c) - 1);
+                if (tMap.containsKey(c) && tMap.get(c) > count.get(c)){
+                    formed--;
+                }
+
+                left++;
+            }
+
+            right++;
+        }
+
+        return result[0] == -1 ? "" : s.substring(result[1], result[2] + 1);
+    }
+}
