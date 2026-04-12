5f9b44b7124757f4727ef20b0a0e857703895169
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-12 12:00:52 +0530

    Add: reverse-bits - submission-1

---
+++ Data Structures & Algorithms/reverse-bits/submission-1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++){
+            result <<= 1;
+            if ((n & 1) == 1){
+                result++;
+            }
+            n >>= 1;
+        }
+
+        return result;
+    }
+}
