343c8d49c88e8751a26aec555a4bd5c04762af1f
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-12 11:55:06 +0530

    Add: sum-of-two-integers - submission-1

---
+++ Data Structures & Algorithms/sum-of-two-integers/submission-1.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int getSum(int a, int b) {
+        int x = Math.abs(a), y = Math.abs(b);
+        if (x < y){
+            return getSum(b, a);
+        }
+
+        int sign = a < 0 ? -1 : 1;
+        if ((a * b) > 0){
+            while (y != 0){
+                int sum = x ^ y;
+                int carry = (x & y) << 1;
+                x = sum;
+                y = carry;
+            }
+        } else {
+             while (y != 0){
+                int sum = x ^ y;
+                int carry = ((~x) & y) << 1;
+                x = sum;
+                y = carry;
+            }
+        }
+
+        return x * sign;
+    }
+}
