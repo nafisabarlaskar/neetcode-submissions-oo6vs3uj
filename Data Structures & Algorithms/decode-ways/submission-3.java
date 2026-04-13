9e8cb79dbdb008b432f0aa52accd33db221caac3
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-13 09:39:46 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length(), dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) == '0' ? 0 : 1;
+        for (int i = 2; i <= n; i++){
+            int first = Integer.parseInt(s.substring(i - 1, i));
+            int second = Integer.parseInt(s.substring(i - 2, i));
+            if (first > 0 && first < 10){
+                dp[i] += dp[i - 1];
+            }
+
+            if (second > 9 && second < 27){
+                dp[i] += dp[i - 2];
+            }
+        }
+
+        return dp[n];
+    }
+}
