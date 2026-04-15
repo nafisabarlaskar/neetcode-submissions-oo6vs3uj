4b295989d31d282cb4bf6324d772b2450965c695
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-15 09:30:19 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1, high = 1;
+        for (int pile : piles){
+            high = Math.max(high, pile);
+        }
+
+        while (low < high){
+            int mid = (low + high)/2;
+            int hours = 0;
+            for (int pile : piles){
+                hours += Math.ceil((double) pile/mid);
+            }
+
+            if (hours <= h){
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
