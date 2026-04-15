b6184992c62cec3570bb8875c34f8c66fe63a087
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-15 09:34:15 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.java
@@ -0,0 +1,28 @@
+class MinStack {
+    Stack<int[]> stack;
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        if (stack.isEmpty()){
+            stack.push(new int[] {val, val});
+        } else {
+            stack.push(new int[] {val, Math.min(stack.peek()[1], val)});
+        }
+    }
+    
+    public void pop() {
+        if (!stack.isEmpty()){
+            stack.pop();
+        }
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
