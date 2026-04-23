4dba063b0f07083571eb94b6be39b043d1429a93
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-23 09:24:11 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.java
@@ -0,0 +1,66 @@
+
+class MyHashSet {
+  private Bucket[] bucketArray;
+  private int keyRange;
+
+  /** Initialize your data structure here. */
+  public MyHashSet() {
+    this.keyRange = 769;
+    this.bucketArray = new Bucket[this.keyRange];
+    for (int i = 0; i < this.keyRange; ++i)
+      this.bucketArray[i] = new Bucket();
+  }
+
+  protected int _hash(int key) {
+    return (key % this.keyRange);
+  }
+
+  public void add(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].insert(key);
+  }
+
+  public void remove(int key) {
+    int bucketIndex = this._hash(key);
+    this.bucketArray[bucketIndex].delete(key);
+  }
+
+  /** Returns true if this set contains the specified element */
+  public boolean contains(int key) {
+    int bucketIndex = this._hash(key);
+    return this.bucketArray[bucketIndex].exists(key);
+  }
+}
+
+
+class Bucket {
+  private LinkedList<Integer> list;
+
+  public Bucket() {
+    list = new LinkedList<Integer>();
+  }
+
+  public void insert(Integer key) {
+    int index = this.list.indexOf(key);
+    if (index == -1) {
+      this.list.addFirst(key);
+    }
+  }
+
+  public void delete(Integer key) {
+    this.list.remove(key);
+  }
+
+  public boolean exists(Integer key) {
+    int index = this.list.indexOf(key);
+    return (index != -1);
+  }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
