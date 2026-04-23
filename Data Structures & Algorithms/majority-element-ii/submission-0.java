6056de5526e1d36c689971d4f68dcaedb494564b
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-23 09:40:58 +0530

    Add: majority-element-ii - submission-0

---
+++ Data Structures & Algorithms/majority-element-ii/submission-0.java
@@ -0,0 +1,47 @@
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        if (nums.length == 0) {
+            return result;
+        }
+
+        int num1 = nums[0], num2 = nums[0], count1 = 0, count2 = 0;
+        for (int num : nums) {
+            if (num == num1) {
+                count1++;
+            } else if (num == num2) {
+                count2++;
+            } else if (count1 == 0) {
+                count1 = 1;
+                num1 = num;
+            } else if (count2 == 0) {
+                count2 = 1;
+                num2 = num;
+            } else {
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+
+        for (int num : nums) {
+            if (num == num1) {
+                count1++;
+            } else if (num == num2) {
+                count2++;
+            }
+        }
+
+        if (count1 > nums.length / 3) {
+            result.add(num1);
+        }
+
+        if (count2 > nums.length / 3) {
+            result.add(num2);
+        }
+
+        return result;
+    }
+}
