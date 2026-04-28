eb0224022b4915d8e40f955c03b9c9d51c2bb092
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-04-28 10:14:09 +0530

    Add: find-the-town-judge - submission-0

---
+++ Data Structures & Algorithms/find-the-town-judge/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        if (trust.length < n - 1) {
+            return -1;
+        }
+
+        int[] outdegree = new int[n + 1];
+        for (int[] t : trust){
+            outdegree[t[0]]--;
+            outdegree[t[1]]++;
+        }
+
+        for (int i = 1; i < n + 1; i++){
+            if (outdegree[i] == n - 1){
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
