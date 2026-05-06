7e807870b64db65b2fd3279a2ded6f0e635542dc
Nafisa Barlaskar <nafisabarlaskar@users.noreply.github.com>
2026-05-06 09:32:24 +0530

    Add: greatest-common-divisor-of-strings - submission-0

---
+++ Data Structures & Algorithms/greatest-common-divisor-of-strings/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+   public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)){
+            return "";
+        }
+
+        return str1.substring(0, gcd(str1.length(), str2.length()));
+    }
+
+    private int gcd(int x, int y){
+        return y == 0 ? x : gcd(y, x % y);
+    }
+}
